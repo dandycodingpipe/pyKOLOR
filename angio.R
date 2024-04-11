d92b7487eefe5d506964ed614012003658e41c04
Chris <christian.a.hernandezf@hotmail.com>
2024-04-11 20:01:09 +0200

    Finished angio and renal graphs. I just need biodistribution data and to complete the abstract and fiure...

--- angio.R
+++ angio.R
@@ -4,27 +4,8 @@ library(dplyr)
 library(readr)
 library(ggplot2)
 
-# load descending thoracic aorta "DTA"
-DTA_data <- vesselLoad("DTA")
-DTA_stats <- vesselStats(DTA_data)
 
-# load suprarenal abdominal aorta "SAA"
-SAA_data <- vesselLoad("SAA")
-SAA_stats <- vesselStats(SAA_data)
-
-# load infra renal aorta "IRA"
-IRA_data <- vesselLoad("IRA")
-IRA_stats <- vesselStats(IRA_data)
-
-# load inferior vena cava "IVC"
-IVC_data <- vesselLoad("IVC")
-IVC_stats <- vesselStats(IVC_data)
-
-# load infra renal vena cava "IRIVC"
-IRVC_data <- vesselLoad("IRVC")
-IRVC_stats <- vesselStats(IRA_data)
-
-vesselLoad <- function(tag, path = "Measurements/") {
+vesselLoad <- function(tag, path = "Vessel_Measurements/") {
   # Construct the pattern to search for files with the specific tag
   pattern <- paste0(tag, "_s[0-9]+\\.csv$")
   
@@ -90,90 +71,78 @@ vesselStats <- function(data) {
   return(list(averages = averages_df, std_devs = std_dev_df))
 }
 
-x = c(0.11, 0.5, 1, 3, 10)
-# angio figure generator function
-plot(x, IVC_stats[[1]]$Signal_Kedge)
+
+# load descending thoracic aorta "DTA"
+DTA_data <- vesselLoad("DTA")
+DTA_stats <- vesselStats(DTA_data)
+
+# load suprarenal abdominal aorta "SAA"
+SAA_data <- vesselLoad("SAA")
+SAA_stats <- vesselStats(SAA_data)
+
+# load infra renal aorta "IRA"
+IRA_data <- vesselLoad("IRA")
+IRA_stats <- vesselStats(IRA_data)
+
+# load inferior vena cava "IVC"
+IVC_data <- vesselLoad("IVC")
+# setting signal of IVC to zero because this is the blood pool expansion effect in the vein, its an incomplete concentration. Salim and I discussed this. These will probably need to be redone...
+#IVC_data[[4]]$Signal_Kedge[1] = 0
+#IVC_data[[5]]$Signal_Kedge[1] = 0
+#IVC_data[[4]]$Signal_HU[1] = 78
+#IVC_data[[5]]$Signal_HU[1] = 70
+
+IVC_stats <- vesselStats(IVC_data)
 
 
-library(ggplot2)
 
-generateAngioPlot <- function(stats_list, column_name, title, x_label, y_label) {
+# load infra renal vena cava "IRIVC"
+IRVC_data <- vesselLoad("IRVC")
+IRVC_stats <- vesselStats(IRVC_data)
+
+
+generateAngioBarsWithError <- function(stats_list, column_name, title, x_label, y_label, y_breaks) {
+  # Assuming Time Points are consistent across datasets
+  time_points <- factor(c(0.11, 0.5, 1, 3, 10)) # Convert to factor for discrete axis
+  
+  vessel_names <- c("SAA", "IRA", "IVC", "IRVC")
+  colors <- c("SAA" = "red", "IRA" = "pink", "IVC" = "blue", "IRVC" = "lightblue")
+  
   # Prepare data for plotting
-  plot_data <- data.frame(X = c(0.11, 0.5, 1, 3, 10))
-  vessel_names <- c("DTA", "SAA", "IRA", "IVC", "IRVC")
+  plot_data <- expand.grid(TimePoint = time_points, Vessel = vessel_names)
+  plot_data$Signal <- unlist(lapply(stats_list, function(x) x$averages[[column_name]]))
+  plot_data$SD <- unlist(lapply(stats_list, function(x) x$std_devs[[column_name]]))
   
-  for (i in seq_along(stats_list)) {
-    vessel_stats <- stats_list[[i]]
-    plot_data[[paste0(vessel_names[i], "_mean")]] <- vessel_stats$averages[[column_name]]
-  }
-  
-  # Melt the data for ggplot, focusing only on mean values
-  plot_data_long <- reshape2::melt(plot_data, id.vars = "X", 
-                                   variable.name = "Vessel_Stat", value.name = "Value")
-  plot_data_long$Vessel <- gsub("_mean$", "", plot_data_long$Vessel_Stat)
-  
-  # Plot
-  ggplot(plot_data_long, aes(x = X, y = Value, group = Vessel, color = Vessel)) +
-    geom_line(size = 2) +  # Thicker lines
-    geom_point(size = 5) +  # Larger points
-    scale_x_continuous(breaks = plot_data$X, labels = as.character(plot_data$X)) +  # Display discrete X values
-    scale_color_manual(values = rainbow(length(vessel_names))) +
+  gg <- ggplot(plot_data, aes(x = TimePoint, y = Signal, fill = Vessel, group = Vessel)) +
+    geom_bar(stat = "identity", position = position_dodge(width = 0.7), width = 0.6) +
+    geom_errorbar(aes(ymin = pmax(Signal - SD, 0), ymax = Signal + SD), width = 0.2, position = position_dodge(width = 0.7)) +
+    scale_fill_manual(values = colors) +
     labs(title = title, x = x_label, y = y_label) +
+    scale_y_continuous(expand = expansion(mult = c(0.003, 0.006)), breaks = y_breaks) +
     theme_minimal() +
-    theme(legend.title = element_blank(),
-          plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),  # Centered and bold title
-          axis.title = element_text(size = 12),
-          axis.text = element_text(size = 10),
-          legend.position = "bottom") +
-    guides(colour = guide_legend(override.aes = list(size=6)))  # Adjust legend key sizes
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      plot.title = element_text(hjust = 0.5), 
+      legend.title = element_blank(), 
+      axis.line = element_line(color="black", size = 1),
+      axis.line.x = element_line(color = "black", size = 1),
+      axis.line.y = element_line(color = "black", size = 1),
+      axis.ticks = element_line(color = "black"), axis.title = element_text(size = 32),
+      panel.border = element_blank(),
+      axis.title.y = element_text(size = 18, margin = margin(r = 20)),
+      axis.title.x = element_text(size = 18, margin = margin(t = 20)),
+      panel.background = element_rect(fill = "white"), axis.text.x = element_text(size = 14),
+      axis.text = element_text(size = 14, margin = margin(r = 20), face = "bold" )
+    )+
+    scale_x_discrete(name = x_label) +
+    geom_vline(xintercept = c(2.5, 4.5), linetype = "dotted", color = "black") # Vertical dotted lines
+  
+  # Print the plot
+  print(gg)
 }
 
 # Example usage
-stats_list <- list(DTA_stats, SAA_stats, IRA_stats, LRA_stats, RRA_stats, IVC_stats)
-generateAngioPlot(stats_list, "CNR_HU", "Vessel CNR (Conventional)", "Minutes", "CNR")
-
-
-
-
-
-
-
-
-plot_data <- data.frame(X = c(0.11, 0.5, 1, 3, 10))
-vessel_names <- c("IRA", "LRA", "RRA")
-vessel_names <- c("IVC", "DTA", "SAA")
-column_name = "Signal_Kedge"
-for (i in seq_along(stats_list)) {
-  vessel_stats <- stats_list[[i]]
-  plot_data[[paste0(vessel_names[i], "_mean")]] <- vessel_stats$averages[[column_name]]
-  plot_data[[paste0(vessel_names[i], "_std")]] <- vessel_stats$std_devs[[column_name]]
-}
-
-# Melt the data for ggplot
-plot_data_long <- reshape2::melt(plot_data, id.vars = "X", 
-                                 variable.name = "Vessel_Stat", value.name = "Value")
-plot_data_long$Vessel <- gsub("(_mean|_std)$", "", plot_data_long$Vessel_Stat)
-plot_data_long$Stat_Type <- ifelse(grepl("_mean$", plot_data_long$Vessel_Stat), "Mean", "Std")
-
-
-
-
-# Filter for mean and standard deviation values
-mean_data <- plot_data_long %>% filter(Stat_Type == "Mean")
-std_data <- plot_data_long %>% filter(Stat_Type == "Std")
-
-# Merge mean and std data frames by X and Vessel
-plot_data <- merge(mean_data, std_data, by = c("X", "Vessel"), suffixes = c("_mean", "_std"))
-
-# Calculate ymin and ymax for error bars
-plot_data$ymin <- plot_data$Value_mean - plot_data$Value_std
-plot_data$ymax <- plot_data$Value_mean + plot_data$Value_std
-
-# Plot
-ggplot(plot_data, aes(x = X, y = Value_mean, color = Vessel)) +
-  geom_line(aes(group = Vessel)) +
-  geom_point() +
-  geom_errorbar(aes(ymin = ymin, ymax = ymax), width = 0.1) +
-  labs(x = "Your X-axis Label", y = "Your Y-axis Label", title = "Your Title") +
-  theme_minimal()
-
+stats_list <- list(SAA_stats, IRA_stats, IVC_stats, IRVC_stats)
+generateAngioBarsWithError(stats_list, "Signal_HU", "Conventional CT Angiography", "Time (min)", "Hounsfield Units (HU)",  y_breaks = c(100, 200, 300, 400 ,500, 600))
+generateAngioBarsWithError(stats_list, "Signal_Kedge", "Gd K-edge Angiography", "Time (min)", "[Gd] (mg/mL)", y_breaks = c(1:10))
--- kidney_figures.R
+++ kidney_figures.R
@@ -1,11 +1,12 @@
 library(dplyr)
 library(readr)
 library(ggplot2)
+library(tidyverse)
 
-renalLoad <- function(tag, path = "Kidney_Measurements/", group) {
-  # Construct the pattern to search for files with the specific tag
-  
-  pattern <- paste0(tag,"_",group, "[0-9]+\\.csv$")
+# Adjusted function to load renal data
+renalLoad <- function(tag, path = "Kidney_Measurements/", group = "") {
+  # Construct the pattern to search for files with the specific tag and group
+  pattern <- paste0(tag, "_", group, "[0-9]+\\.csv$")
   
   # List all files in the directory
   all_files <- list.files(path, pattern = pattern, full.names = TRUE)
@@ -24,52 +25,13 @@ renalLoad <- function(tag, path = "Kidney_Measurements/", group) {
     return(df)
   }
   
-  vesselStats <- function(data) {
-  # Prepare data frames to store the final averages and standard deviations
-  averages_df <- data.frame(matrix(ncol = ncol(data[[1]]), nrow = nrow(data[[1]])))
-  colnames(averages_df) <- colnames(data[[1]])
-  
-  std_dev_df <- data.frame(matrix(ncol = ncol(data[[1]]), nrow = nrow(data[[1]])))
-  colnames(std_dev_df) <- colnames(data[[1]])
-  
-  # Iterate over each row
-  for(row in 1:nrow(data[[1]])) {
-    sums <- vector("numeric", ncol(data[[1]]))
-    squared_sums <- vector("numeric", ncol(data[[1]]))
-    counts <- rep(0, ncol(data[[1]]))
-    
-    # Loop through each data frame in the list
-    for(df in data) {
-      # Check if the row exists in the current data frame
-      if(nrow(df) >= row) {
-        # Convert the row to numeric
-        row_values <- as.numeric(df[row, ])
-        sums <- sums + row_values
-        squared_sums <- squared_sums + (row_values)^2
-        counts <- counts + 1
-      }
-    }
-    
-    # Calculate the averages and standard deviations
-    averages <- sums / counts
-    variances <- (squared_sums - (sums^2 / counts)) / (counts - 1)
-    std_devs <- sqrt(variances)
-    
-    # Store results in the corresponding data frames
-    averages_df[row, ] <- averages
-    std_dev_df[row, ] <- std_devs
-  }
-  
-  return(list(averages = averages_df, std_devs = std_dev_df))
-}
-  
-  
   # Load each file and store in a list
   data_list <- lapply(all_files, readAndPad)
   
   return(data_list)
 }
 
+# Function to calculate stats remains unchanged
 vesselStats <- function(data) {
   # Prepare data frames to store the final averages and standard deviations
   averages_df <- data.frame(matrix(ncol = ncol(data[[1]]), nrow = nrow(data[[1]])))
@@ -108,13 +70,75 @@ vesselStats <- function(data) {
   
   return(list(averages = averages_df, std_devs = std_dev_df))
 }
+
+# Loading renal data and generating descriptive stats
+cortex <- renalLoad("r_cortex", group = "s")
+medulla <- renalLoad("r_medulla", group = "s")
+pelvis <- renalLoad("r_pelvis", group = "s")
+
+cortex_stats <- vesselStats(cortex)
+medulla_stats <- vesselStats(medulla)
+pelvis_stats <- vesselStats(pelvis)
+
+# Function to generate a simple plot for one of the renal areas
+plotRenalData <- function(stats, title) {
+  plot_data <- stats$averages
+  ggplot(plot_data, aes(x = seq_along(Signal_Kedge), y = Signal_Kedge)) +
+    geom_line() +
+    geom_point() +
+    labs(title = title, x = "Time Point", y = "Signal (Kedge)") +
+    theme_minimal()
+}
+
+library(ggplot2)
+library(reshape2) # For melting the data into long format
+
+plotRenalDataBars <- function(stats_list, title, data, ylab, y_breaks) {
+  # Assuming Time Points are consistent across datasets
+  time_points <- c(0.11, 0.5, 1, 3, 10) # Adjust if your time points are different
   
-cortex = renalLoad("r_cortex",path = "Kidney_Measurements/", group = "s")
-medulla = renalLoad("r_medulla",path = "Kidney_Measurements/", group = "s")
-pelvis = renalLoad("r_pelvis",path = "Kidney_Measurements/", group = "s")
-stats_dotarem_c = vesselStats(cortex)
-stats_dotarem_m = vesselStats(medulla)
-stats_dotarem_r = vesselStats(pelvis)
-stats_dotarem_c$averages
-stats_dotarem_m$averages
-stats_dotarem_r$averages
+  renal_areas <- c("Cortex", "Medulla", "Pelvis")
+  
+  # Prepare data for plotting
+  plot_data <- data.frame(TimePoint = rep(time_points, times = length(renal_areas)),
+                          Signal = unlist(lapply(stats_list, function(x) x$averages[[data]])),
+                          SD = unlist(lapply(stats_list, function(x) x$std_devs[[data]])),
+                          RenalArea = rep(renal_areas, each = length(time_points)))
+  
+  # Convert TimePoint to a factor to have discrete bars
+  plot_data$TimePoint <- factor(plot_data$TimePoint, levels = as.character(time_points))
+  
+  gg <- ggplot(plot_data, aes(x = TimePoint, y = Signal, fill = RenalArea)) +
+    geom_bar(stat = "identity", position = position_dodge(width = 0.7), width = 0.6) +
+    geom_errorbar(aes(ymin = Signal - SD, ymax = Signal + SD), width = 0.2, position = position_dodge(width = 0.7)) +
+    scale_fill_manual(values = c("Cortex" = "pink", "Medulla" = "lightblue", "Pelvis" = "lightgreen")) +
+    labs(title = title, x = "Time (min)", y = ylab) +
+    scale_y_continuous(expand = expansion(mult = c(0.003, 0.006)), breaks = y_breaks) +
+    theme_minimal() +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      plot.title = element_text(hjust = 0.5), 
+      legend.title = element_blank(), 
+      axis.line = element_line(color="black", size = 1),
+      axis.line.x = element_line(color = "black", size = 1),
+      axis.line.y = element_line(color = "black", size = 1),
+      axis.ticks = element_line(color = "black"), axis.title = element_text(size = 32),
+      panel.border = element_blank(),
+      axis.title.y = element_text(size = 18, margin = margin(r = 20)),
+      axis.title.x = element_text(size = 18, margin = margin(t = 20)),
+      panel.background = element_rect(fill = "white"), axis.text.x = element_text(size = 14),
+      axis.text = element_text(size = 14, margin = margin(r = 20), face = "bold" )
+    ) +
+    geom_vline(xintercept = c(2.5, 4.5), linetype = "dotted", color = "black") # Vertical dotted lines
+  
+  # Print the plot
+  print(gg)
+}
+
+# Example usage:
+stats_list <- list(cortex_stats, medulla_stats, pelvis_stats)
+plotRenalDataBars(stats_list, "Gd K-edge Renal Elimination", "Signal_Kedge", "[Gd] (mg/mL)", y_breaks = c(1:10) )
+plotRenalDataBars(stats_list, "Conventional CT Angiography", "Signal_HU", "Hounsfield Units (HU)", y_breaks = c(100, 200, 300, 400 ,500, 600))
+#plotRenalDataBars(stats_list, "Gd K-edge Angiography", "CNR_Kedge", "A.U" )
+
